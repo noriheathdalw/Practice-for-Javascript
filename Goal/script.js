706e9f13d91968098b8e9d450543940a989075de
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 15:29:02 +1000

    Create script.js

---
+++ Goal/script.js
@@ -0,0 +1,3 @@
+var goal = "becoming a cubersecurity expert";
+console log("Every day, I'm one step closer to " + goal + ".");
+console log("It's not easy, but with each challenge, I'm learning and growing toward " + goal + ".")
