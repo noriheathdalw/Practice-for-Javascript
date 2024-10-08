7530f7eff3d3fca8d8dd6f63b880073d388d4c2e
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 13:17:09 +1000

    Create script.js

---
+++ Lipstick/script.js
@@ -0,0 +1,3 @@
+var lipstickColor = "Rosy Pink";
+console log("Today's mission: finding the perfect look with " + lipstickColor + "lipstick.")
+console log("but wait... does " + lipstickColor + "match my nails? Decisions, decisions...");
