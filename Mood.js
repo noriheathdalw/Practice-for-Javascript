6b56d810b03fe8052bf669303044484e8ccd265b
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 13:06:35 +1000

    Create Mood.js

---
+++ Mood.js
@@ -0,0 +1,3 @@
+var mood = "super excited";
+console log("Right now, I'm feeling " + mood + " because I found a pizza!");
+console log("Wait, nevermind...I'm now " + mood + "because it's just a picture of pizza.")
